b5479c78bc8f19e42b590f9d49b59836817db876
allezz <aerbay@gmail.com>
2020-05-20 22:39:11 +0200

    Remove not-needed serializer method inside of controller, Add validation test example

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -4,10 +4,5 @@ class Api::ArticlesController < ApplicationController
   def index
     @article = Article.all
     render json: @article
-    # each_serializer: ArticleSerializer
   end
-
-  #   def create_json_response(articles)
-  #     { articles: ArticleSerializer.new(articles) }
-  #   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -7,6 +7,10 @@ RSpec.describe Article, type: :model do
     it { is_expected.to have_db_column :title }
   end
 
+  describe 'Validates' do
+    it { is_expected.to validate_presence_of :title }
+  end
+
   describe 'Factory' do
     it 'should have valid Factory' do
       expect(create(:article)).to be_valid
