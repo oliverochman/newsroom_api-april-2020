18fde51ac04c2815adfafaf33d71a03c94c38caa
allezz <aerbay@gmail.com>
2020-05-21 15:11:15 +0200

    Add create action,Add create route

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -7,11 +7,28 @@ class Api::ArticlesController < ApplicationController
   end
 
   def show
-    begin
-      article = Article.find(params[:id])
-      render json: article, serializer: Article::ShowSerializer
-    rescue
-      render json: { message: "Article with id #{params[:id]} could not be found." }, status: :not_found
+    article = Article.find(params[:id])
+    render json: article, serializer: Article::ShowSerializer
+  rescue StandardError
+    render json: { message: "Article with id #{params[:id]} could not be found." }, status: :not_found
+  end
+
+  def create
+    article = Article.create(article_params)
+    if article.persisted?
+      render json: { id: article.id, message: 'Article successfully created!' }
+    elsif article_params['title'].nil?
+      render json: { message: "Title can't be empty" }, status: 400
+    elsif article_params['body'].nil?
+      render json: { message: "Body can't be empty" }, status: 400
+    else
+      render json: { message: 'Oops! Something went wrong' }, status: 500
     end
+  end
+
+  private
+
+  def article_params
+    params.permit(:title, :body)
   end
 end
