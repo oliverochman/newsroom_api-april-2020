8a9c9d1fc825202021cea00ac08884850855aae2
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-21 13:50:49 +0200

    Create request spec for article creation

---
+++ spec/requests/api/article_create_request_spec.rb
@@ -0,0 +1,50 @@
+RSpec.describe 'Api::Articles :create', type: :request do
+  describe 'Journalist can successfully post an article' do
+    before do
+      post '/api/articles', params: { title: "A title", body: "The body" }
+    end
+
+    it 'has a 200 response' do
+      expect(response).to have_http_status 200
+    end
+
+    it 'responds with article :id' do
+      expect(response_json).to have_key "id"
+    end
+
+    it 'responds with success message' do
+      expect(response_json['message']).to eq "Article successfully created!" 
+    end
+  end
+
+  describe 'Journalist cannot post an article when' do
+    describe 'title is missing' do
+      before do
+        post '/api/articles', params: { body: "The body" }
+      end
+    
+      it 'has a 400 response' do
+        expect(response).to have_http_status 400
+      end
+
+      it 'has a descriptive error message' do
+        expect(response_json['message']).to eq "Title can't be empty"
+      end
+
+    end
+
+    describe 'body is missing' do
+      before do
+        post '/api/articles', params: { title: "A title" }
+      end
+
+      it 'has a 400 response' do
+        expect(response).to have_http_status 400
+      end
+
+      it 'has a descriptive error message' do
+        expect(response_json['message']).to eq "Body can't be empty"
+      end
+    end
+  end
+end
