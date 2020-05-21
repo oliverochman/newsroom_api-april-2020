9fd779473b9f0285c341615edbbe8517d1a011d4
Marcus.Sjoqvist <marcus.sjoqvist@viametrics.com>
2020-05-21 11:26:52 +0200

    add body to articles, add show action, fix factory bot

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -5,4 +5,13 @@ class Api::ArticlesController < ApplicationController
     article = Article.all
     render json: article
   end
+
+  def show
+    begin
+      articlfe = Article.find(params[:id])
+      render json: articlfe
+    rescue
+      render json: { message: "Article with id #{params[:id]} could not be found." }, status: :not_found
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_20_191927) do
+ActiveRecord::Schema.define(version: 2020_05_21_092257) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_05_20_191927) do
     t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
 end
