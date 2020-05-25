ff536a90cfd343b06897809c9ff3de90906ed146
allezz <aerbay@gmail.com>
2020-05-25 18:56:44 +0200

    Change serializer files

--- app/serializers/article/index_serializer.rb
+++ app/serializers/article/index_serializer.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 class Article::IndexSerializer < ActiveModel::Serializer
-  attributes :id, :title, :category, :published_at
+  include Rails.application.routes.url_helpers
+  attributes :id, :title, :category, :published_at, :image
 
   def published_at
     object.created_at.strftime('%F %R')
+  end
+
+  def image
+    return nil unless object.image.attached?
+
+    if Rails.env.test?
+      rails_blob_url(object.image)
+    else
+      object.image.service_url(expires_in: 1.hour, disposition: 'inline')
+    end
   end
 end
