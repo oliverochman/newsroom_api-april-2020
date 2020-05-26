bb2a7755d0f0cf640b3be6e20096607472a09ccb
allezz <aerbay@gmail.com>
2020-05-26 11:31:52 +0200

    PR changes 2

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -5,8 +5,5 @@ FactoryBot.define do
     title { 'this is title' }
     body { 'this is body' }
     category { 'sport' }
-    # trait :with_image do
-    #   image {fixture_file_upload(Rails.root.join("spec","support","assets","test.jpg"), "image/jpg")}
-    # end
   end
 end
